4d344facc362313447a12b6bdb4970d36d5a63c9
Genio22 <104478212+Genio22@users.noreply.github.com>
2025-01-03 09:41:42 +0600

    first commit

--- Lab_9/lab_9_problem_2.c
+++ Lab_9/lab_9_problem_2.c
@@ -36,66 +36,43 @@ int main()
         getchar();
     }
 
+    float most_expensive = book[0].price, lowest_priced = book[0].price;
+    int expensive = 0, lowest = 0;
+    
+    for (int i = 0; i < n; i++)
+    {
+        if (book[i].price > most_expensive){
+            most_expensive = book[i].price;
+            expensive = i;
+        }
+        else if (book[i].price < lowest_priced){
+            lowest_priced = book[i].price;
+            lowest = i;
+        }
+    }
+    
+    printf("Most expensive Book\n");
+        printf("Title: %s", book[expensive].title);
+        printf("Author: %s", book[expensive].author);
+        printf("Price: %.2f\n", most_expensive);
+
+    
+    printf("Lowest priced Book\n");
+        printf("Title: %s", book[lowest].title);
+        printf("Author: %s", book[lowest].author);
+        printf("Price: %.2f\n", lowest_priced);
+
     // Output
+    /*
     for (int i = 0; i < n; i++)
     {
         printf("Book %d\n", i+1);
         printf("Title: %s", book[i].title);
         printf("Author: %s", book[i].author);
         printf("Price: %.2f\n", book[i].price);
-    }
+    }*/
     return 0;
 }
 
-/*#include <stdio.h>
-#include <string.h>
-
-struct mys {
-    char title[100];
-    char author[100];
-    float price;
-};
-
-int main() {
-    int n;
-    printf("Enter the number of books: ");
-    scanf("%d", &n);
-    getchar();  // To consume the newline character after entering the number
-
-    // Array of books to store n books
-    struct mys books[n];
-
-    for (int i = 0; i < n; i++) {
-        printf("Enter details for book %d\n", i + 1);
-
-        // Read the title of the book
-        printf("Book %d title: ", i + 1);
-        fgets(books[i].title, sizeof(books[i].title), stdin);
-        // Remove the newline character added by fgets
-        books[i].title[strcspn(books[i].title, "\n")] = 0;
-
-        // Read the author of the book
-        printf("Book %d author: ", i + 1);
-        fgets(books[i].author, sizeof(books[i].author), stdin);
-        // Remove the newline character added by fgets
-        books[i].author[strcspn(books[i].author, "\n")] = 0;
-
-        // Read the price of the book
-        printf("Book %d price: ", i + 1);
-        scanf("%f", &books[i].price);
-        getchar();  // To consume the newline character after entering the price
-    }
-
-    // Print all books' details
-    for (int i = 0; i < n; i++) {
-        printf("\nBook %d details:\n", i + 1);
-        printf("Title: %s\n", books[i].title);
-        printf("Author: %s\n", books[i].author);
-        printf("Price: %.2f\n", books[i].price);
-    }
-
-    return 0;
-}
-*/
 
 // By sAhAf🙂

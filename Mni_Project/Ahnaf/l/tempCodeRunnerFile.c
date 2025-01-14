2459db7027c0cb2b5cb6372f9cb508470fea3789
Genio22 <104478212+Genio22@users.noreply.github.com>
2025-01-15 03:09:34 +0600

    first commit

---
+++ Mni_Project/Ahnaf/l/tempCodeRunnerFile.c
@@ -0,0 +1,8 @@
+printf("\n\n\n");
+                    printf("**********************************************************************\n");
+                    printf("*  Dear %-58s *\n", c[j].name);
+                    printf("*  Your booking for room number %-35d *\n", roomNumber);
+                    printf("*  The total price for your stay of %-30d days is: %-10d *\n", stayDuration, totalPrice);
+                    printf("*  Thank you for choosing our hotel. We look forward to your stay.   *\n");
+                    printf("**********************************************************************\n");
+                    printf("\n\n\n");

129544192ac032bbf2ae14d32ed73f998154ca7a
Genio22 <104478212+Genio22@users.noreply.github.com>
2025-03-17 13:41:16 +0600

    first commit

--- 2nd Semster (Java code)/Lab4/Lab4/src/main/java/com/mycompany/lab4/Problem7.java
+++ 2nd Semster (Java code)/Lab4/Lab4/src/main/java/com/mycompany/lab4/Problem7.java
@@ -4,10 +4,41 @@
  */
 package com.mycompany.lab4;
 
+import java.util.Scanner;
+
 /**
+ * Write a method that takes an array of integers input. And returns a random
+ * number between 1 and 54, excluding the numbers passed in the argument. The
+ * method header is specified as follows: public static int getRandom(int...
+ * numbers)
  *
- * @author ahnaf
  */
 public class Problem7 {
-    
+
+    public static int getRandom(int[] ex_va) {
+        boolean get = false;
+        int a = -1;
+        while (!get) {
+            a = (int) (Math.random() * (54));
+            for (int elem : ex_va) {
+                if (a != elem && 0 < a) {
+                    get = true;
+                }
+            }
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        //int s = in.nextInt();
+        int e = in.nextInt();
+        int[] ex_va = new int[e];
+        System.out.print("Enter the numbers: ");
+        for (int i = 0; i < e; i++) {
+            ex_va[i] = in.nextInt();
+        }
+        int b = getRandom(ex_va);
+        System.out.println("Result: " + b);
+    }
 }

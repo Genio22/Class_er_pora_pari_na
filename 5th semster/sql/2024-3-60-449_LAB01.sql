6241a44300eb156d0df901cd4a1dd1a37cd85120
Genio22 <104478212+Genio22@users.noreply.github.com>
2026-01-27 16:42:26 +0600

    complete_task

--- 5th semster/sql/2024-3-60-449_LAB01.sql
+++ 5th semster/sql/2024-3-60-449_LAB01.sql
@@ -48,21 +48,19 @@ INSERT INTO instructor_2024360449 VALUES (98345, 'Kim', 'Elec. Eng.', 80000);
 
 --2 b)
 
-INSERT INTO course_2024360449 (course_id, title, dept_name, credits)
-VALUES
-('BIO-101', 'Intro. to Biology', 'Biology', 4),
-('BIO-301', 'Genetics', 'Biology', 4),
-('BIO-399', 'Computational Biology', 'Biology', 3),
-('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', 4),
-('CS-190', 'Game Design', 'Comp. Sci.', 4),
-('CS-315', 'Robotics', 'Comp. Sci.', 3),
-('CS-319', 'Image Processing', 'Comp. Sci.', 3),
-('CS-347', 'Database System Concepts', 'Comp. Sci.', 3),
-('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', 3),
-('FIN-201', 'Investment Banking', 'Finance', 3),
-('HIS-351', 'World History', 'History', 3),
-('MU-199', 'Music Video Production', 'Music', 3),
-('PHY-101', 'Physical Principles', 'Physics', 4);
+INSERT INTO course_2024360449 VALUES ('BIO-101', 'Intro. to Biology', 'Biology', 4);
+INSERT INTO course_2024360449 VALUES ('BIO-301', 'Genetics', 'Biology', 4);
+INSERT INTO course_2024360449 VALUES ('BIO-399', 'Computational Biology', 'Biology', 3);
+INSERT INTO course_2024360449 VALUES ('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', 4);
+INSERT INTO course_2024360449 VALUES ('CS-190', 'Game Design', 'Comp. Sci.', 4);
+INSERT INTO course_2024360449 VALUES ('CS-315', 'Robotics', 'Comp. Sci.', 3);
+INSERT INTO course_2024360449 VALUES ('CS-319', 'Image Processing', 'Comp. Sci.', 3);
+INSERT INTO course_2024360449 VALUES ('CS-347', 'Database System Concepts', 'Comp. Sci.', 3);
+INSERT INTO course_2024360449 VALUES ('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', 3);
+INSERT INTO course_2024360449 VALUES ('FIN-201', 'Investment Banking', 'Finance', 3);
+INSERT INTO course_2024360449 VALUES ('HIS-351', 'World History', 'History', 3);
+INSERT INTO course_2024360449 VALUES ('MU-199', 'Music Video Production', 'Music', 3);
+INSERT INTO course_2024360449 VALUES ('PHY-101', 'Physical Principles', 'Physics', 4);
 
 --------------------------------------------Lab_03 Task-------------------------------------------
 

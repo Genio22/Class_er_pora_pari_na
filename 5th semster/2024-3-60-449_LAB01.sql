229f4f77de7e1f2b849d1c3cd6012ffac43f86ee
Genio22 <104478212+Genio22@users.noreply.github.com>
2026-01-27 14:14:41 +0600

    3push

--- 5th semster/2024-3-60-449_LAB01.sql
+++ 5th semster/2024-3-60-449_LAB01.sql
@@ -23,7 +23,6 @@ create table course_2024360449(
     credits number
 );
 
-
 --2 a)
 
 INSERT INTO instructor_2024360449 VALUES (10101, 'Srinivasan', 'Comp. Sci.', 65000),
@@ -41,15 +40,21 @@ INSERT INTO instructor_2024360449 VALUES (98345, 'Kim', 'Elec. Eng.', 80000);
 
 
 
-INSERT INTO course_2024360449 VALUES
-
-
-
-
-
-
-
-
+INSERT INTO course_2024360449 (course_id, title, dept_name, credits)
+VALUES
+('BIO-101', 'Intro. to Biology', 'Biology', 4),
+('BIO-301', 'Genetics', 'Biology', 4),
+('BIO-399', 'Computational Biology', 'Biology', 3),
+('CS-101', 'Intro. to Computer Science', 'Comp. Sci.', 4),
+('CS-190', 'Game Design', 'Comp. Sci.', 4),
+('CS-315', 'Robotics', 'Comp. Sci.', 3),
+('CS-319', 'Image Processing', 'Comp. Sci.', 3),
+('CS-347', 'Database System Concepts', 'Comp. Sci.', 3),
+('EE-181', 'Intro. to Digital Systems', 'Elec. Eng.', 3),
+('FIN-201', 'Investment Banking', 'Finance', 3),
+('HIS-351', 'World History', 'History', 3),
+('MU-199', 'Music Video Production', 'Music', 3),
+('PHY-101', 'Physical Principles', 'Physics', 4);
 
 
 

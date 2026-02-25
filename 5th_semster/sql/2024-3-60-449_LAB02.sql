ec47501b57a942880d5969ec7f77fa5560db0bb5
Genio22 <104478212+Genio22@users.noreply.github.com>
2026-02-25 05:41:17 +0000

    .

--- 5th_semster/sql/2024-3-60-449_LAB02.sql
+++ 5th_semster/sql/2024-3-60-449_LAB02.sql
@@ -1,61 +1,108 @@
 -- Task 1
 
-CREATE TABLE account(
-    account_no CHAR(5) PRIMARY KEY,
-    balance NUMBER NOT NULL CHECK (balance>=0)
+create table account (
+   account_no char(5) primary key,
+   balance    number not null check ( balance >= 0 )
 );
 
 
-CREATE TABLE customer(
-    customer_no CHAR(5) PRIMARY KEY,
-    customer_name VARCHAR2(20) NOT NULL,
-    customer_city VARCHAR2(10)
+create table customer (
+   customer_no   char(5) primary key,
+   customer_name varchar2(20) not null,
+   customer_city varchar2(10)
 );
 
-CREATE TABLE depositor(
-    account_no CHAR(5),
-    customer_no CHAR(5),
-    PRIMARY KEY (account_no, customer_no)
+create table depositor (
+   account_no  char(5),
+   customer_no char(5),
+   primary key ( account_no,
+                 customer_no )
 );
 
 -- Task 2
 
-ALTER TABLE customer ADD date_of_birth VARCHAR2(20);
+alter table customer add date_of_birth varchar2(20);
 
-ALTER TABLE customer DROP column date_of_birth;
+alter table customer drop column date_of_birth;
 
-ALTER TABLE depositor rename column account_no to a_no;
+alter table depositor rename column account_no to a_no;
 
-ALTER TABLE depositor rename column customer_no to c_no;
+alter table depositor rename column customer_no to c_no;
 
-ALTER TABLE depositor ADD CONSTRAINT depositor_fk1 FOREIGN KEY (a_no) REFERENCES account(account_no);
+alter table depositor
+   add constraint depositor_fk1 foreign key ( a_no )
+      references account ( account_no );
 
-ALTER TABLE depositor ADD CONSTRAINT depositor_fk2 FOREIGN KEY (c_no) REFERENCES customer(customer_no);
+alter table depositor
+   add constraint depositor_fk2 foreign key ( c_no )
+      references customer ( customer_no );
 
 -- Task 3
 
-INSERT INTO account VALUES ('A-101', 12000);
-INSERT INTO account VALUES ('A-102', 6000);
-INSERT INTO account VALUES ('A-103', 2500);
+insert into account values ( 'A-101',
+                             12000 );
+insert into account values ( 'A-102',
+                             6000 );
+insert into account values ( 'A-103',
+                             2500 );
 
-INSERT INTO customer VALUES ('C-101', 'Alice',   'Dhaka');
-INSERT INTO customer VALUES ('C-102', 'Annie',   'Dhaka');
-INSERT INTO customer VALUES ('C-103', 'Bob',     'Chittagong');
-INSERT INTO customer VALUES ('C-104', 'Charlie', 'Khulna');
+insert into customer values ( 'C-101',
+                              'Alice',
+                              'Dhaka' );
+insert into customer values ( 'C-102',
+                              'Annie',
+                              'Dhaka' );
+insert into customer values ( 'C-103',
+                              'Bob',
+                              'Chittagong' );
+insert into customer values ( 'C-104',
+                              'Charlie',
+                              'Khulna' );
 
 
 
-INSERT INTO depositor VALUES ('A-101', 'C-101');
-INSERT INTO depositor VALUES ('A-103', 'C-102');
-INSERT INTO depositor VALUES ('A-103', 'C-104');
-INSERT INTO depositor VALUES ('A-102', 'C-103');
+insert into depositor values ( 'A-101',
+                               'C-101' );
+insert into depositor values ( 'A-103',
+                               'C-102' );
+insert into depositor values ( 'A-103',
+                               'C-104' );
+insert into depositor values ( 'A-102',
+                               'C-103' );
 
 -- Task 4
 
-SELECT customer_name, customer_city FROM customer;
+select customer_name,
+       customer_city
+  from customer;
 
-SELECT DISTINCT customer_city  FROM customer; --unique display
+select distinct customer_city
+  from customer; --unique display
 
-SELECT account_no FROM account WHERE balance >7000;
+select account_no
+  from account
+ where balance > 7000;
 
+select customer_name,
+       customer_no
+  from customer
+ where customer_city = 'Khulna';
 
+select customer_name,
+       customer_no
+  from customer
+ where customer_city not in ( 'Dhaka' );
+
+select customer_name,
+       customer_city
+  from customer
+  join (
+        depositor
+     join (
+      select account_no
+        from account
+       where balance >= 7000
+   ) account
+   on depositor.a_no = account.account_no
+)
+on depositor.c_no = customer.customer_no;

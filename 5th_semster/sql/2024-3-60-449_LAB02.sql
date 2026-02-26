a391ee3770c937c0f04c1392441b81ef96fb8956
Genio22 <104478212+Genio22@users.noreply.github.com>
2026-02-26 16:37:56 +0000

    .

--- 5th_semster/sql/2024-3-60-449_LAB02.sql
+++ 5th_semster/sql/2024-3-60-449_LAB02.sql
@@ -324,10 +324,44 @@ select DIstinct branch_name, branch_city
 --2.Find all account numbers and their balance which are opened in ‘Downtown’ branch or
 -- which have balance in between 600 and 750. (on single table)
 
+select account_number, balance 
+   from account 
+   where branch_name = 'Downtown' and balance between 600 and 750;
 
+--3.Find all account numbers which are opened in a branch located in ‘Rye’ city. (multiple
+-- tables)
 
+select account_number, balance 
+   from account natural join branch
+   where branch_city = 'Rye';
 
+--4. Find all loan numbers which have amount greater than or equal to 1000 
+-- and their customers are living in ‘Harrison’ city. (multiple tables)
 
+select loan_number
+   from loan Natural join borrower natural join customer
+   where amount>= 1000 and customer_city = 'Harrison'
+   order by loan_number asc; --this is optional
 
+--5. Display the account related information based on the descending 
+-- order of the balance. (order by clause)
 
+select *
+   from account
+   order by balance desc;
 
+--6. Display the customer related information in alphabetic order of
+-- customer cities. (order by clause)
+
+select *
+   from customer
+   order by customer_city asc;
+
+--7. Find all customer names who have an account as well as a loan. (intersect)
+select customer_name 
+   from depositor
+   intersect 
+select customer_name
+   from borrower;
+--sql 'RELAQUANTISTEN_SCHEMA_F3KXG/QZN15VQUC!4SD27UC3D1RzQAS6Y8FI@//db.freesql.com:1521/23ai_34ui2'
+                                             

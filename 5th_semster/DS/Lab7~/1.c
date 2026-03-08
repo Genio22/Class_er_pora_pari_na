3819650b36a5848d95613a04adbb389b3ddf00a1
Genio22 <104478212+Genio22@users.noreply.github.com>
2026-03-08 06:44:53 +0000

    .

---
+++ 5th_semster/DS/Lab7~/1.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *left;
+    struct node *right;
+} node;
+
+node *create_node(int value)
+{
+    node *newnode = (node *)malloc(sizeof(node));
+    newnode->data = value;
+    newnode->left = NULL;
+    newnode->right = NULL;
+    return newnode;
+}
+
+
+node *insert(node *root, int value)
+{
+
+    if (root == NULL)
+    {
+        return create_node(value);
+    }
+ 
+    if (value < root->data)
+    {
+        root->left = insert(root->left, value);
+    }
+    else if (value > root->data)
+    {
+        root->right = insert(root->right, value);
+    }
+
+    
+    return root;
+}
+void inorder(node *root)
+{
+    if (root != NULL)
+    {
+        inorder(root->left);
+        printf(" %d ", root->data);
+        inorder(root->right);
+    }
+}
+
+void postorder(node *root)
+{
+    if (root != NULL)
+    {
+        postorder(root->left);
+        postorder(root->right);
+        printf(" %d ", root->data);
+    }
+}
+
+int height(node *root){
+    if (root == NULL){
+        return 0;
+    }    
+    return (height(root->left)>height(root->right)) ? height(root->left) +1 : height(root->right)+1;
+}
+
+int main()
+{
+
+    return 0;
+}

555d14913db72e28707b44a6ffe84ab791f7b1cd
Genio22 <relaquantisten@gmail.com>
2026-06-29 22:14:20 +0600

    lab task done

--- 6th semster/Task1/p1.py
+++ 6th semster/Task1/p1.py
@@ -1,3 +1,11 @@
+file_path = "try.txt"
+
+with open(file_path, "w", encoding="utf-8") as f:
+    data = []
+    for i in range(2, 300, 3):
+        f.write(f"{str(i)}\n")
+
+
 def linear_search (list, target):
     index = 0
     for i in list:
@@ -7,15 +15,16 @@ def linear_search (list, target):
 
     return -1
 
-if __name__ == "__main__":
-    file_path = "try.txt"
+
+def lin_ser_with_file(path):
+
 
     target = int(input("Enter a number from 2 - 300, to find it's index\n> ").strip())
 
     index = -1
-    with open(file_path, "r", encoding="utf-8") as f:
+    with open(path, "r", encoding="utf-8") as f:
         data = f.readlines()
-        index = linear_search (int(data), target)
+        index = linear_search ([int(i.strip()) for i in data], target)
 
 
     if index != -1:
@@ -24,3 +33,56 @@ if __name__ == "__main__":
     else:
         print("Sorry")
         print(f"{target} is not found!")
+
+
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n-1):
+        min_idx = i
+        
+        for j in range(i+1, n):
+            if arr[j]< arr[min_idx]:
+                min_idx = j
+        
+        arr[i], arr[min_idx]= arr[min_idx], arr[i]
+
+
+def binary_search(arr, target):
+    low = 0
+    high = len(arr)-1
+    while low <= high:
+        mid = low + (high - low) // 2
+
+        if arr[mid] == target:
+            return mid + 1
+        elif arr[mid] < target:
+            low = mid + 1
+        else:
+            high = mid -1
+    return -1
+
+
+
+
+if __name__ == "__main__":
+
+    print("\n=====Task2====")
+    print("=====Linear_search with from file====\n\n")
+    file_path = "/run/media/genio/Sahaf Software_s/ALL/Class_er_pora_pari_na/6th semster/Task1/try.txt"
+    lin_ser_with_file(file_path)
+
+    print("\n=====Task5====")
+    print("=====Selection_sort====\n\n")
+    arr = [64, 25, 12, 22, 11]
+    selection_sort(arr)
+    print("Sorted array is:", arr)
+
+    print("\n=====Task3====")
+    print("=====Binary_search====\n\n")
+    
+    result = binary_search(arr, int(input("Enter a number:  "))) 
+    if result != -1:
+        print("Element is present at index ", result)
+    else:
+        print("Element is not present in array")
+    
